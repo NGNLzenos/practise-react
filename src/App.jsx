c8a7e0bab56a50a14f5214da4abf755aea9edcb0
羅文森 <vincentale.21@gmail.com>
2025-12-25 12:12:20 +0800

    u

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
-import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+
+    (async () => {
+
+      const res = await axios.get('https://randomuser.me/api/')
+      console.log(res.data)
+
+    })()
+
+  }, [])
 
   return (
     <>
